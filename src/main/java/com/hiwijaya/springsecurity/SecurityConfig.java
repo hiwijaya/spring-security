3afcf9fdd2f6bb25851efbc901c1f80e4982a8e2
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2021-05-04 16:11:37 +0700

    added httpsBasic()

--- src/main/java/com/hiwijaya/springsecurity/SecurityConfig.java
+++ src/main/java/com/hiwijaya/springsecurity/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.hiwijaya.springsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,12 +9,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Happy Indra Wijaya
@@ -50,7 +46,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests()
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/restricted").hasRole("ADMIN")
                 .anyRequest().authenticated()
