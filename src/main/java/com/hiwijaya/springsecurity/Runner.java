45916126111cbe3d3ea7ee815a924d2e0da32807
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2021-06-28 20:21:18 +0700

    clear unused codes

--- src/main/java/com/hiwijaya/springsecurity/Runner.java
+++ src/main/java/com/hiwijaya/springsecurity/Runner.java
@@ -1,11 +1,7 @@
 package com.hiwijaya.springsecurity;
 
-import com.hiwijaya.springsecurity.entity.User;
-import com.hiwijaya.springsecurity.service.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,24 +10,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class Runner implements ApplicationRunner {
 
-    @Autowired
-    private AuthService authService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
-        User admin = User.builder()
-                .username("hiwijaya")
-                .password(passwordEncoder.encode("mesosfer!"))
-                .role(Role.ROLE_ADMIN)
-                .build();
-
-//        authService.register(admin);
-
+        // do something
     }
 
 }
--- src/main/java/com/hiwijaya/springsecurity/SecurityConfig.java
+++ src/main/java/com/hiwijaya/springsecurity/SecurityConfig.java
@@ -1,15 +1,11 @@
 package com.hiwijaya.springsecurity;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -30,22 +26,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public CustomAccessDeniedHandler accessDeniedHandler() {
         return new CustomAccessDeniedHandler();
     }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Autowired
-    private UserDetailsService userDetailsService;      // injected by AuthService
-
-    @Autowired
-    public void authConfig(AuthenticationManagerBuilder auth) throws Exception {
-        auth.parentAuthenticationManager(authenticationManagerBean())
-                .userDetailsService(userDetailsService);
-    }
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
